a413e69f6be415d44d2b7fff6a824c868c10212d
Cameron Rodriguez <dev@camrod.me>
2023-03-12 19:55:21 -0400

    Add fonts and update some content

--- assets/js/wayback_redirect.js
+++ assets/js/wayback_redirect.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const waybackDirect = document.getElementById("referrer-page");
+    const waybackDirect = document.getElementById("404-referrer");
     if (!document.baseURI.includes("404.html")) {
         waybackDirect.setAttribute("href", `https://web.archive.org/web/*/${document.URL}`)
     }
